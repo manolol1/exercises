d33327f37b5f9f98231841d95ab4ab776214f050
manolol1 <manolol@mailbox.org>
2024-03-12 22:08:26 +0100

    Finish 19_04

--- 19/EX_19_04_Patternmatching.java
+++ 19/EX_19_04_Patternmatching.java
@@ -10,5 +10,26 @@ public class EX_19_04_Patternmatching {
    public static void main(String[] args){
       int[] ar = {5, 3, 8, 4, 6, 2, 2, 7, 8, 3, 8, 5, 5, 9, 4, 3, 5};
       int[] pattern = {3, 8, 5};
+      
+      int firstMatchIndex = -1;
+      
+      for (int i = 0; i < ar.length - pattern.length && firstMatchIndex == -1; i++) {
+         boolean matchPossible = true;
+         for (int j = 0; j < pattern.length && matchPossible; j++) {
+            if (ar[i + j] != pattern[j]) {
+               matchPossible = false;
+            }
+         }
+         if (matchPossible) {
+            firstMatchIndex = i;
+         }
+      }
+      
+      // output
+      if (firstMatchIndex == -1) {
+         System.out.println("Kuerzeres Array kommt nicht vor.");
+      } else {
+         System.out.println("Kuerzeres Array kommt ab Position " + firstMatchIndex + " vor.");
+      }
    }
 }
