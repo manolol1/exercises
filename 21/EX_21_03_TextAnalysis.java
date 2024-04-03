349d0ee7ff6fc9ea0be7ca73f6825ca684e0ca8c
Mario Ebenhofer <20230009@students.htl-perg.ac.at>
2024-04-03 12:53:42 +0200

    finish 21_03; start 21_04

--- 21/EX_21_03_TextAnalysis.java
+++ 21/EX_21_03_TextAnalysis.java
@@ -12,9 +12,41 @@ public class EX_21_03_TextAnalysis {
    public static void main(String[] args){
       Scanner s = new Scanner(System.in);
       
+      int[] count = new int[26]; // A = 0; B = 1; ...
+      
       final String INPUT;
       
       System.out.print("Eingabe: ");
-      INPUT = s.nextLine();
+      INPUT = s.nextLine().toUpperCase();
+      
+      // count letters
+      for (int i = 0; i < INPUT.length(); i++) {
+         for (int j = 0; j < 26; j++) {
+            if (INPUT.charAt(i) == 65 + j) {
+               count[j]++;
+            }
+         }
+      }
+      
+      // print statistic
+      System.out.println("Statistik:");
+      for (int i = 0; i < count.length; i++) {
+         System.out.print((char) (65 + i) + ":");
+         System.out.printf("%6d x", count[i]);
+         System.out.println("\t\t" + graphicalNum(count[i]));
+      }
+   }
+   
+   private static String graphicalNum(int n) {
+      StringBuilder result = new StringBuilder(n + n/5);
+      
+      for (int i = 0; i < n; i++) {
+         if (i % 5 == 0 && i != 0) {
+            result.append(" ");
+         }
+         result.append("|");
+      }
+      
+      return result.toString();
    }
 }
--- 21/EX_21_04_MaxSum.java
+++ 21/EX_21_04_MaxSum.java
@@ -12,5 +12,20 @@ public class EX_21_04_MaxSum {
    public static void main(String[] args){
       Scanner s = new Scanner(System.in);
       
+      final int LENGTH = 10;
+      final int LB = -10, UB = 10;
+      
+      int[] ar = randomArray(LENGTH, LB, UB);
    }
+   
+   private static int[] randomArray(int length, int lb, int ub) {
+      int[] ar = new int[length];
+      
+      for (int i = 0; i < length; i++) {
+         ar[i] = (int) (Math.random() * (ub - lb + 1) + lb);
+      }
+      
+      return ar;
+   }
+
 }
