62f1e118034e15b402ecbbe333625ea0aae0ae3c
manolol1 <manolol@mailbox.org>
2024-04-06 20:48:55 +0200

    Update EX_21_04_MaxSum.java

--- 21/EX_21_04_MaxSum.java
+++ 21/EX_21_04_MaxSum.java
@@ -3,19 +3,37 @@
 * Datum:          03.04. 2024
 * HUE:            21
 * Bsp:            04
-* Beschreibung:   
+* Beschreibung:   Groesste Teilsumme in Array finden
 *********************************************/
-
-import java.util.Scanner;
 
 public class EX_21_04_MaxSum {
    public static void main(String[] args){
-      Scanner s = new Scanner(System.in);
-      
       final int LENGTH = 10;
       final int LB = -10, UB = 10;
       
       int[] ar = randomArray(LENGTH, LB, UB);
+      //int[] ar = {5, -7, 3, 6, 4, -5, 4, 2, -8, 4};
+      
+      int startIndex = 0, endIndex = 0, maxSum = ar[0];
+      int tempStart = 0, tempSum = 0;
+      
+      System.out.println(arrayToString(ar));
+      
+      for (int i = 0; i < ar.length; i++) {
+         tempSum += ar[i];
+         if (tempSum > maxSum) {
+            maxSum = tempSum;
+            startIndex = tempStart;
+            endIndex = i;
+         }
+         
+         if (tempSum < 0) {
+            tempSum = 0;
+            tempStart = i + 1;
+         }
+      }
+      
+      System.out.printf("Die groesste Teilsumme betraegt %d und reicht von Position %d bis Position %d.", maxSum, startIndex, endIndex);
    }
    
    private static int[] randomArray(int length, int lb, int ub) {
@@ -26,6 +44,16 @@ public class EX_21_04_MaxSum {
       }
       
       return ar;
+   }
+   
+   private static String arrayToString(int[] ar) {
+      String result = "" + ar[0];
+      
+      for (int i = 1; i < ar.length; i++) {
+         result += " " + ar[i];
+      }
+      
+      return result;
    }
 
 }
