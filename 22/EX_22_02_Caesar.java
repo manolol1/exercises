0c5c5395451503ebe6af334a4fa06a0de73fbc85
manolol1 <manolol@mailbox.org>
2024-04-13 17:34:14 +0200

    Update EX_22_02_Caesar.java

--- 22/EX_22_02_Caesar.java
+++ 22/EX_22_02_Caesar.java
@@ -12,6 +12,7 @@ public class EX_22_02_Caesar {
    public static void main(String[] args){
       Scanner s = new Scanner(System.in);
       
+      // test encrypt() and decrypt() with a random String
       int k = -3;
       String str = encrypt("Hallo ABCD XYZ ?", k);
       System.out.println(str);
