89ddef4488f6efc70c4dbd7743c9a7f2ca1a1d75
Mario Ebenhofer <20230009@students.htl-perg.ac.at>
2024-05-06 12:33:13 +0200

    update

--- 25/EX_25_02_Methods.java
+++ 25/EX_25_02_Methods.java
@@ -6,11 +6,8 @@
 * Description:   Methods
 *********************************************/
 
-import java.util.Scanner;
-
 public class EX_25_02_Methods {
    public static void main(String[] args){
-      Scanner s = new Scanner(System.in);
       
    }
 }
