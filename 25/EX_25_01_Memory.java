b95c97e8b2d80e73e4df5c52a2141ecdbe662482
Mario Ebenhofer <20230009@students.htl-perg.ac.at>
2024-05-08 09:45:49 +0200

    fix 25_01

--- 25/EX_25_01_Memory.java
+++ 25/EX_25_01_Memory.java
@@ -13,19 +13,19 @@ public class EX_25_01_Memory {
       Scanner s = new Scanner(System.in);
       
       final int WIN_CONDITION = 12;
+      final int LB = 1, UB = 20;
       
       int[] numbers = new int[WIN_CONDITION];
       
       for (int i = 0; i < WIN_CONDITION; i++) {
+         clear();
          // get new number
-         System.out.printf("%-6sZahl:\t\t", i + 1 + ".");
-         numbers[i] = s.nextInt();
-         
-         System.out.println();
+         numbers[i] = random(LB, UB);
+         System.out.printf("%-6s Zahl: %6d\n\n", i + 1 + ".", numbers[i]);
       
          // ask user to repeat all numbers
          for (int j = 0; j <= i; j++) {
-            System.out.printf("%-6sEingabe:\t", j + 1 + ".");
+            System.out.printf("%-6s Eingabe: \t", j + 1 + ".");
             if (s.nextInt() != numbers[j]) {
 
                System.out.printf("\nFehler! Du hast nur %d Zahlen richtig wiedergegeben.\n", i);
@@ -36,5 +36,13 @@ public class EX_25_01_Memory {
          System.out.println();
       }
       System.out.println("Juhu, du hast gewonnen.");
+   }
+   
+   private static int random(int lb, int ub) {
+      return (int) (Math.random() * (ub - lb + 1)) + lb;
+   }
+   
+   private static void clear() {
+      System.out.print("\n".repeat(20));
    }
 }
