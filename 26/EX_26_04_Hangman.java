92a2ebc14a11d4c9e64faefa858e58352da11c56
manolol1 <manolol@mailbox.org>
2024-05-16 17:36:35 +0200

    Add ascii art

--- 26/EX_26_04_Hangman.java
+++ 26/EX_26_04_Hangman.java
@@ -12,5 +12,8 @@ public class EX_26_04_Hangman {
    public static void main(String[] args) {
       Scanner s = new Scanner(System.in);
       
+      final String[] ascii = new ASCII().hangman;
+      
+      System.out.println(ascii[1]);
    }
 }
