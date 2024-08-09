b3dc24db83ffdfeb893e97ac13696eb546e745d6
Mario Ebenhofer <manolol@mailbox.org>
2024-08-09 18:54:26 +0200

    move files from 1st grade to new branch, remove files in main

--- 20/EX_20_04_SnailRace/Constants.java
+++
@@ -1,8 +0,0 @@
-/********************************************
-* Zusatzklasse
-* Aufgabe: EX_20_04_SnailRace
-*********************************************/
-
-public class Constants {
-   public static final int FIELD_SIZE = 32;
-}
--- Sonstiges/c/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-   int num;
-   char ch;
-   
-   //scanf("%d %c", &num, &ch);
-   printf("%d %c", num, ch);
-}
