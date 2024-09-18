03fd9f6dce4b9935cc04380b92ac559b49f376be
Mario Ebenhofer <20230009@students.htl-perg.ac.at>
2024-09-18 08:48:29 +0200

    1

--- HUE_1/src/Main.java
+++ HUE_1/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner s = new Scanner(System.in);
+
+
+    }
+
+    public static int intInput(Scanner s, String name, int min, int max) {
+        System.out.printf("%s (%d-%d): ", name, min, max);
+        int input = 0;
+
+        try {
+            input = s.nextInt();
+        } catch (Exception e) {
+            System.out.println("An error occured while parsing your input. Try again!\n");
+        }
+
+        if (input < min || input > max) {
+            System.out.printf("Input must be between %d and %d. Try again!\n\n");
+            return intInput(s, name, min, max);
+        }
+
+        return input;
+
     }
 }
