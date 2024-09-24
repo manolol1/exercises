2ac20a89d56e53fd3370d6721e71efeb8824ace9
Mario Ebenhofer <manolol@mailbox.org>
2024-09-24 21:12:23 +0200

    1

---
+++ HUE_1/src/GraphicsMode/BoardCanvas.java
@@ -0,0 +1,20 @@
+package GraphicsMode;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BoardCanvas  extends JPanel {
+    public BoardCanvas() {
+        this.setBackground(Color.BLACK);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setPaint(Color.white);
+
+        g2d.drawLine(0, 0, this.getWidth(), this.getHeight());
+    }
+}
--- HUE_1/src/GraphicsMode/Frames/MainFrame.java
+++ HUE_1/src/GraphicsMode/Frames/MainFrame.java
@@ -1,14 +1,61 @@
 package GraphicsMode.Frames;
 
 import General.Constants;
+import GraphicsMode.BoardCanvas;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
+    private final JPanel optionsPanel;
+    private final JPanel controlsPanel;
+
+    private final JButton newBoardButton;
+    private final JButton exitButton;
+
     public MainFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(Constants.DEFAULT_MAIN_FRAME_SIZE);
-        this.setTitle("Conway's Game of Life");
+        this.setMinimumSize(new Dimension(600, 400));
+        this.setTitle("Game of Life");
+
+        /* Options Panel */
+        optionsPanel = new JPanel();
+        optionsPanel.setPreferredSize(new Dimension(120, 0));
+        optionsPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        optionsPanel.setBackground(Color.BLUE);
+
+        // new board button
+        newBoardButton = new JButton("New Board");
+        newBoardButton.setPreferredSize(new Dimension(100, 30));
+        newBoardButton.setFocusable(false);
+        newBoardButton.addActionListener(e -> {
+            // TODO: Trigger creation of a new board
+        });
+        optionsPanel.add(newBoardButton);
+
+        // exit button
+        exitButton = new JButton("Exit");
+        exitButton.setPreferredSize(new Dimension(100, 30));
+        exitButton.setFocusable(false);
+        exitButton.addActionListener(e -> {
+            System.exit(0);
+        });
+        optionsPanel.add(exitButton);
+
+        /* Controls Panel */
+        controlsPanel = new JPanel();
+        controlsPanel.setPreferredSize(new Dimension(0, 80));
+        controlsPanel.setBackground(Color.YELLOW);
+
+        /* Board Canvas */
+        BoardCanvas boardCanvas = new BoardCanvas();
+
+        // Add every panel to the frame
+        this.add(optionsPanel, BorderLayout.WEST);
+        this.add(controlsPanel, BorderLayout.SOUTH);
+        this.add(boardCanvas, BorderLayout.CENTER);
+
         this.setVisible(true);
     }
 }
