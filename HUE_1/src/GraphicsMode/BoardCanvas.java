22df42c3119679e33b95363ed2ffe7763870569b
Mario Ebenhofer <manolol@mailbox.org>
2024-09-24 21:20:07 +0200

    1

--- HUE_1/src/GraphicsMode/BoardCanvas.java
+++ HUE_1/src/GraphicsMode/BoardCanvas.java
@@ -10,11 +10,15 @@ public class BoardCanvas  extends JPanel {
 
     @Override
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        super.paintComponent(g); // draw panel (background)
         Graphics2D g2d = (Graphics2D) g;
 
         g2d.setPaint(Color.white);
 
         g2d.drawLine(0, 0, this.getWidth(), this.getHeight());
+    }
+
+    public void redraw() {
+        this.repaint();
     }
 }
