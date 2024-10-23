86b1568a889b599b3f32752e3766985461f87427
Mario Ebenhofer <manolol@mailbox.org>
2024-10-23 19:51:39 +0200

    5

--- HUE_5/src/EX1/Main.java
+++ HUE_5/src/EX1/Main.java
@@ -17,7 +17,11 @@ public class Main {
         printDigitReverse(1234);
         System.out.println();
 
-        System.out.printf("Summe: %d", sum(10));
+        System.out.printf("Summe: %d\n", sum(10));
+        System.out.printf("Produkt: %d\n", product(11, 9));
+
+        char[] text = "Hello World!".toCharArray();
+        reverseText(text);
     }
 
     /**
@@ -64,5 +68,33 @@ public class Main {
         if (n <= 0) return 0;
 
         return sum(n - 1) + n;
+    }
+
+    /**
+     * calculate product of two numbers recursively
+     * @param n factor 1
+     * @param m factor 2
+     * @return result
+     */
+    public static int product(int n, int m) {
+        if (m <= 0) return 0;
+
+        return product(n, m - 1) + n;
+    }
+
+    /**
+     * reverse a char[] and print it
+     * @param text char[] to reverse
+     */
+    public static void reverseText(char[] text) {
+        if (text.length == 0) return;
+
+        System.out.print(text[text.length - 1]);
+
+        // create new array without last element
+        char[] newText = new char[text.length - 1];
+        System.arraycopy(text, 0, newText, 0, text.length - 1);
+
+        reverseText(newText);
     }
 }
