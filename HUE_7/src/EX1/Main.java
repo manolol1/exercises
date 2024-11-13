4c55e4bacddd71cdb60b62f8252e8727b2082880
Mario Ebenhofer <manolol@mailbox.org>
2024-11-13 21:19:36 +0100

    7

--- HUE_7/src/EX1/Main.java
+++ HUE_7/src/EX1/Main.java
@@ -2,19 +2,70 @@ package EX1;
 
 public class Main {
     public static void main(String[] args) {
-        MyTime[] times = new MyTime[5];
+        MyTime[] times = new MyTime[6];
 
         times[0] = new MyTime(2, 40, 20);
         times[1] = new MyTime(-2, 40, 20);
+        times[2] = new MyTime(12, 45, 10);
+        times[3] = new MyTime(1, 14, 10);
+        times[4] = new MyTime(42, 91, 22);
+        times[5] = MyTime.difference(times[3], times[2]);
 
-        printArray(times);
+        times[2].add(times[3]);
+        times[2].subtract(times[3]);
 
-        System.out.println(times[0].getTotalSeconds());
+        printTimesArray(times, true);
+
+        sortTimes(times);
+        System.out.println();
+
+        printTimesArray(times, true);
+
+        System.out.println();
+
+        System.out.println(MyTime.now().getGreeting() + ", " + System.getProperty("user.name") + "!");
     }
 
+    /**
+     * Print an array of any type of Object (line-by-line)
+     * This requires the Object to implement its own toString() method.
+     * @param ar Array of Objects
+     */
     public static void printArray(Object[] ar) {
         for (Object obj : ar) {
             System.out.println(obj);
+        }
+    }
+
+    /**
+     * Print an Array of MyTimes
+     * @param ar Array to print
+     * @param printTotalSeconds if true, the total seconds of a time will be printed next to the hh:mm:ss format.
+     */
+    public static void printTimesArray(MyTime[] ar, boolean printTotalSeconds) {
+        for (MyTime t : ar) {
+            System.out.println(t.toString(printTotalSeconds));
+        }
+    }
+
+    /**
+     * Sort an array of MyTimes
+     * @param ar Array to sort
+     */
+    public static void sortTimes(MyTime[] ar) {
+        MyTime temp;
+        for (int i = 0; i < ar.length; i++) {
+            if (ar[i] == null) ar[i] = new MyTime(0, 0, 0);
+            int minIndex = i;
+            for (int j = i; j < ar.length; j++) {
+                if (ar[j].getTotalSeconds() < ar[minIndex].getTotalSeconds()) {
+                    minIndex = j;
+                }
+            }
+
+            temp = ar[i];
+            ar[i] = ar[minIndex];
+            ar[minIndex] = temp;
         }
     }
 }
--- HUE_7/src/EX1/MyTime.java
+++ HUE_7/src/EX1/MyTime.java
@@ -1,17 +1,23 @@
 package EX1;
 
-public class MyTime {
-    public static final int SECONDS_PER_DAY = 86400;
+import java.time.ZonedDateTime;
 
+/**
+ * Class for storing and processing a time in 24-hour format
+ */
+public class MyTime {
     private int hours, minutes, seconds;
 
-    /**
-     * Class for storing and processing a time in 24-hour format
-     */
     public MyTime(int hours, int minutes, int seconds) {
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
+        normalize();
+    }
+
+    public MyTime(int totalSeconds) {
+        this.seconds = totalSeconds;
+        // minutes and hours will be automatically calculated by normalize()
         normalize();
     }
 
@@ -20,27 +26,131 @@ public class MyTime {
     }
 
     /**
-     * Normalize time (calculate overflows)
+     * Normalize time (calculate overflows and account for negative values)
      */
     public void normalize() {
-        hours = Math.abs(hours);
-        minutes = Math.abs(minutes);
-        seconds = Math.abs(seconds);
-        int totalSeconds = getTotalSeconds() % SECONDS_PER_DAY;
-
-        hours = 0;
-        while (totalSeconds >= 3600) {
-            hours++;
-            totalSeconds -= 3600;
+        // seconds
+        if (seconds >= 60 || seconds < 0) {
+            minutes += seconds / 60;
+            seconds = seconds % 60;
+            if (seconds < 0) {
+                seconds += 60;
+                minutes--;
+            }
         }
 
-        minutes = 0;
-        while (totalSeconds >= 60) {
-            minutes++;
-            totalSeconds -= 60;
+        //  minutes
+        if (minutes >= 60 || minutes < 0) {
+            hours += minutes / 60;
+            minutes = minutes % 60;
+            if (minutes < 0) {
+                minutes += 60;
+                hours--;
+            }
         }
 
-        seconds = totalSeconds;
+        // hours
+        if (hours >= 24 || hours < 0) {
+            hours = hours % 24;
+            if (hours < 0) {
+                hours += 24;
+            }
+        }
+    }
+
+    /**
+     * Add a time to this time.
+     * @param t2 Time to add
+     */
+    public void add(MyTime t2) {
+        // prevent NullPointerException
+        if (t2 == null) {
+            t2 = new MyTime(0, 0, 0);
+        }
+
+        hours += t2.hours;
+        minutes += t2.minutes;
+        seconds += t2.seconds;
+        normalize();
+    }
+
+    /**
+     * Subtract a time from this time.
+     * @param t2 Time to add
+     */
+    public void subtract(MyTime t2) {
+        // prevent NullPointerException
+        if (t2 == null) {
+            t2 = new MyTime(0, 0, 0);
+        }
+
+        hours -= t2.hours;
+        minutes -= t2.minutes;
+        seconds -= t2.seconds;
+        normalize();
+    }
+
+    /**
+     * Calculate the difference of two times
+     * @param t1 first time
+     * @param t2 seconds time
+     * @return Difference
+     */
+    public static MyTime difference(MyTime t1, MyTime t2) {
+        return new MyTime(Math.abs(t1.getTotalSeconds() - t2.getTotalSeconds()));
+    }
+
+    /**
+     * Convert MyTime to String
+     * @return MyTime as String in hh:mm:ss format
+     */
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    /**
+     * Make a deep copy of this MyTime
+     * @return copy
+     */
+    public MyTime getCopy() {
+        return new MyTime(hours, minutes, seconds);
+    }
+
+    /**
+     * Get current time (in the correct timezone)
+     * @return current time
+     */
+    public static MyTime now() {
+        ZonedDateTime currentTime = ZonedDateTime.now();
+        return new MyTime(currentTime.getHour(), currentTime.getMinute(), currentTime.getSecond());
+    }
+
+    /**
+     * Convert MyTime to String
+     * @param withSeconds if enabled, the total seconds will be added next to the formatted time.(hh:mm:ss  --  totalSeconds)
+     * @return MyTime as String in hh:mm:ss format.
+     */
+    public String toString(boolean withSeconds) {
+        if (withSeconds) {
+            return String.format("%02d:%02d:%02d  --  Total Seconds: %d", hours, minutes, seconds, getTotalSeconds());
+        } else {
+            return toString();
+        }
+    }
+
+    public String getGreeting() {
+        if (hours <= 5 || hours >= 21) {
+            return "Good night";
+        } else if (hours <= 10) {
+            return "Good morning";
+        } else if (hours <= 13) {
+            return "Good noon";
+        } else if (hours <= 16) {
+            return "Good afternoon";
+        } else {
+            return "Good night";
+        }
     }
 
     public int getHours() {
@@ -68,10 +178,5 @@ public class MyTime {
     public void setSeconds(int seconds) {
         this.seconds = seconds;
         normalize();
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 }
