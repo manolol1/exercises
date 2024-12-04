c7fa49ce9280acb5a00a2a4c91fd44feec33e8d1
Mario Ebenhofer <20230009@students.htl-perg.ac.at>
2024-12-04 11:45:12 +0100

    9

--- HUE_9/src/Ex1/Main.java
+++ HUE_9/src/Ex1/Main.java
@@ -13,29 +13,95 @@ public class Main {
         s2.add(1);
         System.out.println("s2: "+s2); // {1,2,3}
 
-        //s1.add(s2);
+        s1.add(s2);
         System.out.println("s1: "+s1); // {1,2,3}
         s1.add(5);
         System.out.println("s1: "+s1); // {1,2,3,5}
         s1.delete(5);
         System.out.println("s1: "+s1); // {1,2,3}
-//
-//        s1.add(5);
-//        s1.add(8);
-//        s1.add(3);
-//        s1.add(5);
-//        System.out.println("s1: "+s1); // {1,2,3,5,8}
-//        s1.delete(1);
-//        s1.delete(2);
-//        s1.delete(3);
-//        System.out.println("s1: "+s1); // {5,8}
-//
-//        s1.union(s2);
-//        System.out.println("s1: "+s1); // {1,2,3,5,8}
-//
-//        s2.delete(3);
-//        s1.intersection(s2);
-//        System.out.println("s1: "+s1); // {1,2}
 
+        s1.add(5);
+        s1.add(8);
+        s1.add(3);
+        s1.add(5);
+        System.out.println("s1: "+s1); // {1,2,3,5,8}
+        s1.delete(1);
+        s1.delete(2);
+        s1.delete(3);
+        System.out.println("s1: "+s1); // {5,8}
+
+        s1.union(s2);
+        System.out.println("s1: "+s1); // {1,2,3,5,8}
+
+        s2.delete(3);
+        s1.intersection(s2);
+        System.out.println("s1: "+s1); // {1,2}
+
+        MySet[] sets = new MySet[4];
+        sets[0] = new MySet();
+        sets[1] = new MySet();
+        sets[2] = new MySet();
+        sets[3] = new MySet();
+        sets[0].add(5);
+        sets[0].add(4);
+        sets[0].add(-7);
+        sets[0].add(12);
+        sets[0].add(22);
+        sets[0].add(-28);
+        sets[1].add(6);
+        sets[1].add(12);
+        sets[1].add(4);
+        sets[1].add(-8);
+        sets[1].add(62);
+        sets[1].add(8);
+        sets[1].add(8);
+        sets[2].add(52);
+        sets[2].add(3);
+        sets[2].add(1);
+        sets[2].add(-8);
+        sets[2].add(9);
+        sets[2].add(6);
+        sets[2].add(3);
+        sets[3].add(44);
+        sets[3].add(23);
+        sets[3].add(9);
+        sets[3].add(6);
+        sets[3].add(12);
+        sets[3].add(-30);
+        sets[3].add(-17);
+
+        sortSets(sets);
+        printSets(sets);
+    }
+
+    /**
+     * Sort set array by total sums
+     * @param ar array to sort
+     */
+    public static void sortSets(MySet[] ar) {
+        int min;
+        MySet temp;
+
+        for (int i = 0; i < ar.length; i++) {
+            min = i;
+            for (int j = i; j < ar.length; j++) {
+                if (ar[j].getSum() < ar[min].getSum()) {
+                    min = j;
+                }
+            }
+
+            temp = ar[i];
+            ar[i] = ar[min];
+            ar[min] = temp;
+        }
+    }
+
+    /**
+     * Print all sets with their sum
+     */
+    public static void printSets(MySet[] ar) {
+        for (MySet set : ar) {
+            System.out.println(set.toStringWithSum());
+        }
     }
 }
--- HUE_9/src/Ex1/MySet.java
+++ HUE_9/src/Ex1/MySet.java
@@ -20,6 +20,17 @@ public class MySet {
             newSet[set.length] = value;
             set = newSet;
         }
+        sort();
+    }
+
+    /**
+     * Add another set to the set, if the values don't already exist
+     * @param set2 will be added to the set
+     */
+    public void add(MySet set2) {
+        for (int n : set2.getSet()) {
+            this.add(n);
+        }
     }
 
     /**
@@ -27,11 +38,44 @@ public class MySet {
      * @param value will be deleted from the set
      */
     public void delete(int value) {
-        if (contains(value)) {
+        int index = find(value);
+
+        if (index != -1) {
             int[] newSet = new int[set.length - 1];
-            int index = find(value);
-            System.arraycopy(set, 0, newSet, 0, index - 1);
-            System.arraycopy(set, index, newSet, index, newSet.length);
+            // copy values from (old) set into newSet
+            int i = 0, j = 0;
+            while (j < newSet.length) {
+                if (set[i] == value) {
+                    // skip deleted value
+                    i++;
+                }
+                newSet[j] = set[i];
+
+                i++;
+                j++;
+            }
+
+            set = newSet;
+        }
+    }
+
+    /**
+     * Add another set to the set, if the values don't already exist (same as add())
+     * @param set2 will be added to the set
+     */
+    public void union(MySet set2) {
+        this.add(set2);
+    }
+
+    /**
+     * Remove all values from this set, that aren't contained in another
+     * @param set2 set to compare to
+     */
+    public void intersection(MySet set2) {
+        for (int value : this.set) {
+            if (!set2.contains(value)) {
+                this.delete(value);
+            }
         }
     }
 
@@ -56,8 +100,44 @@ public class MySet {
         return -1;
     }
 
+    /**
+     * Sort the array in ascending order
+     */
+    public void sort() {
+        Arrays.sort(set);
+    }
+
+    /**
+     * @return total sum of all values in set
+     */
+    public int getSum() {
+        int sum = 0;
+
+        for (int n : set) {
+            sum += n;
+        }
+
+        return sum;
+    }
+
+    public int[] getSet() {
+        return set;
+    }
+
+    /**
+     * Check if set is empty
+     * @return whether the set is empty
+     */
+    public boolean isEmpty() {
+        return set.length == 0;
+    }
+
     @Override
     public String toString() {
         return Arrays.toString(set).replaceAll("\\[", "{").replaceAll("]", "}");
+    }
+
+    public String toStringWithSum() {
+        return toString() + " - " + this.getSum();
     }
 }
