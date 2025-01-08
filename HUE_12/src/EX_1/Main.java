3e4b9f8989473314efcb52bfd0779ba4373de3b2
Mario Ebenhofer <20230009@students.htl-perg.ac.at>
2025-01-08 11:39:45 +0100

    12

--- HUE_12/src/EX_1/Main.java
+++ HUE_12/src/EX_1/Main.java
@@ -1,4 +1,42 @@
 package EX_1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Main {
+    public static void main(String[] args) {
+        String s = "Hallo ich bin ein String der geteilt werden soll.";
+
+        System.out.println(Arrays.toString(splitTokenizer(s)));
+        System.out.println(Arrays.toString(splitScanner(s)));
+        System.out.println(Arrays.toString(splitString(s)));
+    }
+
+    public static String[] splitTokenizer(String s) {
+        ArrayList<String> tokens = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(s);
+
+        while (tokenizer.hasMoreTokens()) {
+            tokens.add(tokenizer.nextToken());
+        }
+
+        return tokens.toArray(new String[0]);
+    }
+
+    public static String[] splitScanner(String s) {
+        ArrayList<String> tokens = new ArrayList<>();
+        Scanner scanner = new Scanner(s);
+
+        while (scanner.hasNext()) {
+            tokens.add(scanner.next());
+        }
+
+        return tokens.toArray(new String[0]);
+    }
+
+    public static String[] splitString(String s) {
+        return s.split("[^\\p{L}]");
+    }
 }
--- HUE_12/src/EX_2/Main.java
+++ HUE_12/src/EX_2/Main.java
@@ -1,4 +1,41 @@
 package EX_2;
 
+import JavaUtils.*;
+
 public class Main {
+    public static void main(String[] args) {
+        int repetitions = 100;
+        int stringLength = 10000;
+
+        System.out.println(Timer.benchmarkFunctionExecTime(Function -> testStringBuilder(stringLength), repetitions));
+        System.out.println(Timer.benchmarkFunctionExecTime(Function -> testStringBuffer(stringLength), repetitions));
+        System.out.println(Timer.benchmarkFunctionExecTime(Function -> testString(stringLength), repetitions));
+    }
+
+    private static String testStringBuilder(int stringLength) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < stringLength; i++) {
+            sb.append((char) JavaUtils.random(65, 90));
+        }
+
+        return sb.toString();
+    }
+
+    private static String testStringBuffer(int stringLength) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < stringLength; i++) {
+            sb.append((char) JavaUtils.random(65, 90));
+        }
+
+        return sb.toString();
+    }
+
+    private static String testString(int stringLength) {
+        String s = new String();
+        for (int i = 0; i < stringLength; i++) {
+            s += (char) JavaUtils.random(65, 90);
+        }
+
+        return s;
+    }
 }
